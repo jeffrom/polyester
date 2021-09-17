7d4d7e27075f40ccd255b14744a601c50d7b0320
Jeff Martin <jeffmartin@gmail.com>
2021-09-17 01:44:00 -0700

    git-repo: impl --version

--- operator/gitop/repo.go
+++ operator/gitop/repo.go
@@ -3,7 +3,6 @@ package gitop
 import (
 	"bytes"
 	"errors"
-	"fmt"
 	"os"
 	"os/exec"
 	"regexp"
@@ -13,6 +12,7 @@ import (
 
 	"github.com/jeffrom/polyester/operator"
 	"github.com/jeffrom/polyester/state"
+	"github.com/jeffrom/polyester/stdio"
 )
 
 type RepoOpts struct {
@@ -48,9 +48,10 @@ func (op Repo) Info() operator.Info {
 }
 
 func (op Repo) GetState(octx operator.Context) (state.State, error) {
+	std := stdio.FromContext(octx.Context)
 	opts := op.Args.(*RepoOpts)
 	st := state.State{}
-	// fmt.Printf("git-repo: GetState opts: %+v\n", opts)
+	std.Debugf("git-repo: GetState opts: %+v\n", opts)
 
 	currRefStr, ref, err := getCurrentCommit(octx, octx.FS.Join(opts.Dest))
 	if err != nil || currRefStr == "" {
@@ -71,7 +72,7 @@ func (op Repo) GetState(octx operator.Context) (state.State, error) {
 		}
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
-		// fmt.Println("+ git", cmd.Args)
+		std.Debug("+ git", cmd.Args)
 		if err := cmd.Run(); err != nil {
 			return st, err
 		}
@@ -88,6 +89,7 @@ func (op Repo) GetState(octx operator.Context) (state.State, error) {
 }
 
 func (op Repo) Run(octx operator.Context) error {
+	std := stdio.FromContext(octx.Context)
 	opts := op.Args.(*RepoOpts)
 	ctx := octx.Context
 
@@ -113,7 +115,7 @@ func (op Repo) Run(octx operator.Context) error {
 			args = append(args, "--branch", opts.Ref)
 		}
 		cmd := exec.CommandContext(ctx, "git", args...)
-		fmt.Println("+", cmd.Args)
+		std.Info("+", cmd.Args)
 		cmd.Stdin = os.Stdin
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
@@ -121,7 +123,15 @@ func (op Repo) Run(octx operator.Context) error {
 			return err
 		}
 	} else if opts.Version != "" && opts.Version != currRef {
-		panic("version not implemented")
+		cmd := exec.CommandContext(ctx, "git", "checkout", opts.Version)
+		std.Info("+", cmd.Args)
+		cmd.Dir = octx.FS.Join(opts.Dest)
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		if err := cmd.Run(); err != nil {
+			return err
+		}
 	} else if opts.Ref == "" || opts.Ref == "HEAD" {
 		latestRef, err := getLatestCommit(octx, octx.FS.Join(opts.Dest), ref)
 		if err != nil {
@@ -129,7 +139,7 @@ func (op Repo) Run(octx operator.Context) error {
 		}
 		if currRef != latestRef {
 			cmd := exec.CommandContext(ctx, "git", "-c", "advice.detachedHead=false", "checkout", latestRef)
-			fmt.Println("+", cmd.Args)
+			std.Info("+", cmd.Args)
 			cmd.Dir = octx.FS.Join(opts.Dest)
 			cmd.Stdin = os.Stdin
 			cmd.Stdout = os.Stdout
